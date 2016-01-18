2326d209b8e4d53d787bdf10df6279a2231d1cd4
taojiaxiang <774828671@qq.com>
2016-01-18 16:34:49 +0800

    tang SB

---
+++ TaoAndTang/TaoAndTang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaoAndTang
+//
+//  Created by qianfeng on 16/1/18.
+//  Copyright © 2016年 TAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaoAndTang/TaoAndTang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaoAndTang
+//
+//  Created by qianfeng on 16/1/18.
+//  Copyright © 2016年 TAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
